5890b1183dc05bbf6dec88115b6f58f7550fa508
Tofik Abdu <tofikabdu2002@gmail.com>
2023-08-01 14:41:35 +0300

    created: task_3

---
+++ Task_3 Palindrome Check/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+class Palindrome
+{
+    public static void Main(string[] args)
+    {
+        string word = Console.ReadLine()!;
+        if (word.Length == 0)
+        {
+            Console.WriteLine("please enter a valid word");
+        }
+        else
+        {
+            string cleanWord = Regex.Replace(word, @"\p{P}", "").ToLower();
+
+            bool result = CheckPalindrome(cleanWord);
+            Console.WriteLine(result);
+        }
+        
+    }
+
+    public static bool CheckPalindrome(string word)
+    {
+        var i = 0;
+        var j = word.Length - 1;
+
+        while (i < j)
+        {
+            if (word[i] != word[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
