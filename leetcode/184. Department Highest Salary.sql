1098b444a3264680f89f607314e602b745aedbbb
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-08-07 15:43:22 +0300

    Create 184. Department Highest Salary.sql

---
+++ leetcode/184. Department Highest Salary.sql
@@ -0,0 +1,19 @@
+select 
+    d.name as Department,
+    e.name as Employee,
+    e.salary as Salary
+
+from 
+    Employee as e
+join 
+    Department as d
+    on e.departmentId = d.id
+    
+where (d.id, e.salary) in 
+(
+    select e.departmentId , max(e.salary)
+    from Employee as e 
+    group by e.departmentId
+)
+    
+
